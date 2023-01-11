d50e19f11b90fd4d06743fc81beacdeaafc36447
Alexandru Mitcul <alexmitcul@Alexandrus-MacBook-Air.local>
2023-01-11 23:54:07 +0000

    unicode support done

---
+++ src/client_bonus.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amitcul <amitcul@student.42porto.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 14:37:17 by amitcul           #+#    #+#             */
+/*   Updated: 2023/01/11 23:48:56 by amitcul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minitalk_bonus.h"
+
+void	send_character(int pid, int character)
+{
+	char	index;
+
+	ft_printf("%c", character);
+	index = 0;
+	while (index < 8)
+	{
+		if (character & (1 << index))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		index++;
+		usleep(200);
+	}
+}
+
+void	send_message(int pid, char *message)
+{
+	int	i;
+
+	i = 0;
+	while (message[i] != '\0')
+	{
+		send_character(pid, (int)message[i]);
+		i++;
+	}
+}
+
+int	get_pid(char *pid)
+{
+	int	i;
+
+	i = 0;
+	while (pid[i] != '\0')
+	{
+		if (ft_isdigit(pid[i]) == 0)
+			return (-1);
+		i++;
+	}
+	return (ft_atoi(pid));
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc != 3)
+	{
+		ft_printf("Wrong input!\n");
+		ft_printf("Try: './client [PID] [message]'\n");
+		return (0);
+	}
+	pid = get_pid(argv[1]);
+	if (pid == -1)
+	{
+		ft_printf("Wrong PID.\n");
+		return (0);
+	}
+	send_message(pid, argv[2]);
+	return (0);
+}
---
+++ src/server_bonus.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amitcul <amitcul@student.42porto.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 01:16:01 by amitcul           #+#    #+#             */
+/*   Updated: 2023/01/11 23:50:09 by amitcul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minitalk_bonus.h"
+
+void	signal_handler(int signal)
+{
+	static int	value = 0;
+	static char	index = 0;
+
+	if (signal == SIGUSR1)
+	value = value | (0x01 << index);
+	if (index == 7)
+	{
+		ft_printf("%c", value);
+		value = 0;
+		index = 0;
+		return ;
+	}
+	index++;
+}
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+
+	(void)argv;
+	if (argc != 1)
+	{
+		printf("Wrong input!\n");
+		printf("Try: './server'\n");
+		return (0);
+	}
+	pid = getpid();
+	ft_printf("PID: %d\n", pid);
+	while (1)
+	{
+		signal(SIGUSR1, &signal_handler);
+		signal(SIGUSR2, &signal_handler);
+	}
+	return (0);
+}
